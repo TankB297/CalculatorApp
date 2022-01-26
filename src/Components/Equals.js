342c4bdde54cd8821e118883437385250dbdd38f
TankB297 <tankjerry511@gmail.com>
2022-01-27 00:09:53 +0700

    TankB Update 26/01/2022

--- src/Components/Equals.js
+++ src/Components/Equals.js
@@ -1,14 +1,24 @@
-import { Component, memo } from "react";
+import { memo, useState, useEffect } from "react";
 import "../StyleFormat/Equals.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
-class Equals extends Component {
-  render() {
-    const { userInput } = this.props;
-    return (
-      <div className="displayEquals">
-        <p>{userInput}</p>
-      </div>
-    );
-  }
+function Equals(props) {
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user && !user.emailVerified) {
+      alert("You need to verify your email!");
+      navigate("/");
+    }
+  }, [user]);
+
+  return (
+    <div className="displayEquals">
+      <p>{props.userInput}</p>
+    </div>
+  );
 }
 export default memo(Equals);
